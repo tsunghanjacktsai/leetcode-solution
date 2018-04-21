45082797c4fdb78fe2eb816478862e75c4865b2e
Jack <34185019+jack870131@users.noreply.github.com>
2018-04-21 14:12:25 +0800

    Update ValidParentheses.java

--- src/stack/ValidParentheses.java
+++ src/stack/ValidParentheses.java
@@ -11,14 +11,13 @@ public class ValidParentheses {
 	}
 
 	public static boolean isValid(String s) {
-		//�N���w�r�Ŵ��N���ŭ�
 		while(true){
 			
 			String temp=s.replace("[]","").replace("{}","").replace("()", "");
 			
 			if(temp.length() < s.length()) {
 				
-				s = temp;//��r�Ŧ�L�k�A�Y���(�Ŧr�Ŧ�)�פ�`��
+				s = temp;
 			} else {
 				
 				break; 
