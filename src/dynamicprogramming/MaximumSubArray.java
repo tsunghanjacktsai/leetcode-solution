2dcaa1e2738faa9fe80d73f836ab1e29fcff71d7
Jack <34185019+jack870131@users.noreply.github.com>
2018-05-02 15:26:24 +0800

    Update MaximumSubArray.java

--- src/dynamicprogramming/MaximumSubArray.java
+++ src/dynamicprogramming/MaximumSubArray.java
@@ -1,12 +1,6 @@
 package dynamicprogramming;
 
 public class MaximumSubArray {
-
-	public static void main(String[] args) {
-		int[] arr = {-2,1,-3,4,-1,-2,-1,-5,-4};
-		int result = maxSubArray(arr);
-		System.out.println(result);
-	}
 	
 	public static int maxSubArray(int[] nums) {
 		int max = Integer.MIN_VALUE;
@@ -30,5 +24,11 @@ public class MaximumSubArray {
 		}
 		
 		return soFar;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {-2,1,-3,4,-1,-2,-1,-5,-4};
+		int result = maxSubArray(arr);
+		System.out.println(result);
 	}
 }
