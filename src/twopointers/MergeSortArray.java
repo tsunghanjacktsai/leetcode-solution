f90356eca560fd9756f2f1856ff6369db2ddb1eb
Jack <34185019+jack870131@users.noreply.github.com>
2018-05-02 15:35:37 +0800

    Update MergeSortArray.java

--- src/twopointers/MergeSortArray.java
+++ src/twopointers/MergeSortArray.java
@@ -1,10 +1,6 @@
 package twopointers;
 
 public class MergeSortArray {
-
-	public static void main(String[] args) {
-		
-	}
 	
 	public static void merge(int[] nums1, int m, int[] nums2, int n) {
 		int x = m - 1;
