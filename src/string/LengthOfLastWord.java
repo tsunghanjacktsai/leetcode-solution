182e05cf3812b234319c611586bd342950776fef
Jack <34185019+jack870131@users.noreply.github.com>
2018-05-02 15:32:38 +0800

    Update LengthOfLastWord.java

--- src/string/LengthOfLastWord.java
+++ src/string/LengthOfLastWord.java
@@ -1,17 +1,17 @@
 package string;
 
 public class LengthOfLastWord {
-
-	public static void main(String[] args) {
-		String input = "Hello World";
-		int result = lengthOfLastWord(input);
-		
-		System.out.println(result);
-	}
 	
 	public static int lengthOfLastWord(String s) {
 		String str = s.trim();
 		int length = str.length() - str.lastIndexOf(' ') - 1;
 		return length;
+	}
+	
+	public static void main(String[] args) {
+		String input = "Hello World";
+		int result = lengthOfLastWord(input);
+		
+		System.out.println(result);
 	}
 }
