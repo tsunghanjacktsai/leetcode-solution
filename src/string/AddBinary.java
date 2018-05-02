61a33966724d659e88da20432429413423eedcd4
Jack <34185019+jack870131@users.noreply.github.com>
2018-05-02 15:33:38 +0800

    Update AddBinary.java

--- src/string/AddBinary.java
+++ src/string/AddBinary.java
@@ -1,13 +1,6 @@
 package string;
 
 public class AddBinary {
-
-	public static void main(String[] args) {
-		String a = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
-		String b = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
-		String sum = addBinary(a, b);
-		System.out.println(sum);
-	}
 	
 	public static String addBinary(String a, String b) {
 		StringBuilder builder = new StringBuilder();
@@ -23,4 +16,10 @@ public class AddBinary {
 		return builder.toString();
 	}
 
+	public static void main(String[] args) {
+		String a = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+		String b = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
+		String sum = addBinary(a, b);
+		System.out.println(sum);
+	}
 }
