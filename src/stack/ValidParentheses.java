30a37c66b0aef9b14c2442320336b92f77828b3b
Jack <34185019+jack870131@users.noreply.github.com>
2018-05-02 15:31:11 +0800

    Update ValidParentheses.java

--- src/stack/ValidParentheses.java
+++ src/stack/ValidParentheses.java
@@ -2,14 +2,6 @@ package stack;
 
 public class ValidParentheses {
 
-	public static void main(String[] args) {
-
-		String s = "[()]";
-		boolean b = isValid(s);
-
-		System.out.println(b);
-	}
-
 	public static boolean isValid(String s) {
 		while(true){
 			
@@ -26,4 +18,11 @@ public class ValidParentheses {
 		return s.isEmpty();
 	}
 
+	public static void main(String[] args) {
+
+		String s = "[()]";
+		boolean b = isValid(s);
+
+		System.out.println(b);
+	}
 }
